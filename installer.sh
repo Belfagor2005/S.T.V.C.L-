bccfdc60b35734c7feb7c27d45a32974af1a146c
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-27 16:34:20 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -86,7 +86,7 @@ if [ $OSTYPE != "DreamOs" ]; then
 fi
 sleep 2
 
-wget --no-check-certificate --no-cache --no-dns-cache 'https://github.com/Belfagor2005/S.T.V.C.L-/archive/refs/heads/main.tar.gz'
+wget --no-check-certificate 'https://github.com/Belfagor2005/S.T.V.C.L-/archive/refs/heads/main.tar.gz'
 tar -xzf main.tar.gz
 cp -r 'stvcl--main/usr' '/'
 set +e
