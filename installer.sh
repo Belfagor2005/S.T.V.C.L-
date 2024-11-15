5f90dac5e80f9091bb470d20eaf1bf41600ababa
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-11-15 20:19:51 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ##setup command=wget -q "--no-check-certificate" https://raw.githubusercontent.com/Belfagor2005/S.T.V.C.L-/main/installer.sh -O - | /bin/sh
-exec > >(tee -a /tmp/stvcl_debug.txt) 2>&1
-set -x
+
 ## Only This 2 lines to edit with new version ######
 version='1.4'
 changelog='\nAdd Locale Language\nFix Upgrade'
